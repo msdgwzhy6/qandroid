f3a7974ed1174d059f42e16afc6d8ef7daada904
Jay <fjtianxia@gmail.com>
2014-03-21 00:06:49 +0800

    init project.

---
+++ README.md
@@ -0,0 +1,9 @@
+QAndroid
+======
+Quick android:搭建Linux下快速Android开发环境
+
+
+tools
+source
+android-templates
+
---
+++ source/tools/qandroid/src/com/jayfeng/qandroid/tool/Enter.java
@@ -0,0 +1,11 @@
+package com.jayfeng.qandroid.tool;
+
+public class Enter {
+
+	public static void main(String[] args) {
+		for (String string : args) {
+			System.out.println(string);
+		}
+	}
+
+}
